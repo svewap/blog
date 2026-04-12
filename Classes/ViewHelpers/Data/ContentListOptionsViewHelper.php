325cb520087aab6b44274aba55524303e81b2b38
Sven Wappler <wappler@wappler.systems>
2026-04-12 02:06:49 +0200

    Fix: Build complete fake tt_content record for TYPO3 v14.2 RecordFactory compatibility
    TYPO3 v14.2 RecordFactory requires all system fields (language, workspace,
    capabilities) to be present in tt_content records. The ContentListOptionsViewHelper
    now initializes all TCA column defaults plus required system fields to prevent
    IncompleteRecordException when rendering blog sidebar plugins.

--- Classes/ViewHelpers/Data/ContentListOptionsViewHelper.php
+++ Classes/ViewHelpers/Data/ContentListOptionsViewHelper.php
@@ -30,13 +30,45 @@ class ContentListOptionsViewHelper extends AbstractViewHelper
         $settings = GeneralUtility::makeInstance(ConfigurationManagerInterface::class)
             ->getConfiguration(ConfigurationManagerInterface::CONFIGURATION_TYPE_SETTINGS, 'blog');
         $listTypeConfiguration = $settings['contentListOptions'][$arguments['listType']] ?? [];
+
+        // Build a fake tt_content record with all system fields required by TYPO3 v14 RecordFactory.
+        // Initialize all TCA columns with empty defaults to prevent IncompleteRecordException.
+        $defaults = [];
+        foreach (($GLOBALS['TCA']['tt_content']['columns'] ?? []) as $fieldName => $fieldConfig) {
+            $defaults[$fieldName] = $fieldConfig['config']['default'] ?? '';
+        }
+        // System fields not in TCA columns but required by RecordFactory
+        $defaults = array_merge($defaults, [
+            'uid' => Constants::LISTTYPE_TO_FAKE_UID_MAPPING[$arguments['listType']] ?? 0,
+            'pid' => 0,
+            'crdate' => 0,
+            'tstamp' => 0,
+            'deleted' => 0,
+            'hidden' => 0,
+            'sorting' => 0,
+            'sys_language_uid' => 0,
+            'l18n_parent' => 0,
+            'l10n_source' => 0,
+            't3ver_oid' => 0,
+            't3ver_wsid' => 0,
+            't3ver_state' => 0,
+            't3ver_stage' => 0,
+            'starttime' => 0,
+            'endtime' => 0,
+            'fe_group' => '',
+            'editlock' => 0,
+            'rowDescription' => '',
+            'colPos' => 0,
+        ]);
+
         $data = array_merge(
+            $defaults,
             $listTypeConfiguration,
             [
                 'uid' => Constants::LISTTYPE_TO_FAKE_UID_MAPPING[$arguments['listType']] ?? 0,
                 'CType' => $arguments['listType'] ?? '',
                 'layout' => $listTypeConfiguration['layout'] ?? '0',
-                'frame_class' => $listTypeConfiguration['frame_class'] ?? 'default'
+                'frame_class' => $listTypeConfiguration['frame_class'] ?? 'default',
             ]
         );
 
